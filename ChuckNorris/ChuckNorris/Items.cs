e68b48c11d41633912172d697d4f7fca107071dd
Gustavo Brandão <gustavobrandao@corpugbvr.ugb.edu.br>
2019-01-03 16:22:37 -0200

    First commit.

---
+++ ChuckNorris/ChuckNorris/Items.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckNorris
+{
+    class Items
+    {
+        public string category { get { return this.category; } set { if (String.IsNullOrEmpty(value)) { this.category = "other"; } } }
+
+        public string icon_url { get; set; }
+
+        public string id { get; set; }
+
+        public string url { get; set; }
+
+        public string value { get; set; }
+    }
+}
--- ChuckNorris/ChuckNorris/MainPage.xaml.cs
+++ ChuckNorris/ChuckNorris/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +14,24 @@ namespace ChuckNorris
         public MainPage()
         {
             InitializeComponent();
+        }
+        
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                var response = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+                var items = JsonConvert.DeserializeObject<Items>(response);
+
+                value.Text = items.value;
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("ATTENTION", ex.ToString(), "OK");
+            }
         }
     }
 }
